96173c106fc679c4e2f587b6615427fe38cc85bf
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-05-03 03:47:16 +0900

    サンプルコード作成

--- back_end/gateToFront.py
+++ back_end/gateToFront.py
@@ -4,9 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 import sys   
-from sqlalchemy import Column, Integer, String   
+from sqlalchemy import Column, Integer, String  
+import datetime 
 
-dbname   = 'comichaiku'
+dbname   = 'twitredb'
 # this socket is going to be very different on a Windows computer
 socket   = '?unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock'
 dbname   = dbname + socket
@@ -32,24 +33,34 @@ session = scoped_session(
 Base = declarative_base()
 Base.query = session.query_property()
   
-class Chaikus(Base):  
-    __tablename__ = 'chaikus'  
+#   DateTime型がわからないため、とりあえずコメントアウトしてある。
+class TwitterApiTbl(Base):  
+    __tablename__ = 'twitter_api_tbl'  
     id = Column('id', Integer, primary_key=True)  
-    top = Column('top', String(100))  
-    center = Column('center', String(100))  
-    bottom = Column('bottom', String(100)) 
+    id_str = Column('id_str', String(100))  
+    screen_name = Column('screen_name', String(100))  
+    created_at = Column('created_at', String(100)) 
+    # create_time = Column('create_time', datetime) 
+    text = Column('text', String(1000)) 
+    trend = Column('trend', String(100)) 
+    user_id = Column('user_id', String(100)) 
+    user_id_str = Column('user_id_str', String(100)) 
+    use_name = Column('use_name', String(100)) 
+    # sys_create_date = Column('sys_create_date', datetime) 
+    hidden_flag = Column('hidden_flag', Integer) 
+    delete_flag = Column('delete_flag', Integer) 
     query = session.query_property()
 
     @classmethod
-    def getChaikus(self):
+    def getAll(self):
         return self.query.all()
   
 def main(args):  
     # Base と ENGINE をDB接続設定からインポート
     Base.metadata.create_all(bind=ENGINE)   
-    chaikus = Chaikus.getChaikus()
-    for chaiku in chaikus:
-        print(chaiku.id, chaiku.top)
+    twiApiData = TwitterApiTbl.getAll()
+    for data in twiApiData:
+        print(data.id, data.text)
         
   
 if __name__ == '__main__':  
