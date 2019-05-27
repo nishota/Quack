b45ca7874fdfa677706dc3f9a9d02ff96e4789df
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-05-28 08:31:00 +0900

    [front] anime設定をカプセル化

--- front_end/src/app/tweet-card/tweet-card.component.ts
+++ front_end/src/app/tweet-card/tweet-card.component.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 import { DisplayService } from '../service/display.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DisplayCardComponent } from '../display-card/display-card.component';
+import { AnimeMode, AnimeSetting } from '../model/anime-setting.model';
 
 @Component({
   selector: 'app-tweet-card',
@@ -15,12 +16,9 @@ import { DisplayCardComponent } from '../display-card/display-card.component';
 })
 export class TweetCardComponent implements OnInit {
   @Input() tweet: TweetData;
-  tweetCardId = 'target';
-  tweetCardCN = 'center target';
-  userPage = environment.twitterURL;
   card = new Card();
+  animeSetting: AnimeSetting;
   display = 'none';
-  zIndex = 0;
 
   interval;
 
@@ -33,51 +31,15 @@ export class TweetCardComponent implements OnInit {
 
   ngOnInit() {
     if (this.tweet) {
-      const tweetNum = this.tweet.TweetId.slice(-3);
-      this.tweetCardId += tweetNum;
-      this.tweetCardCN += tweetNum;
-      this.userPage += this.tweet.User;
-      const id = Number(tweetNum);
-      if (Number.isInteger(id)) {
-        this.zIndex = id;
-      }
+      this.animeSetting = new AnimeSetting(this.card, this.tweet, this.TIME);
       this.start();
     }
   }
 
   start() {
     this.interval = setInterval(() => {
-      // ランダムに動かす
-      // this.TIME = 3000;
-      // this.card.setCoordRandom();
-      // this.card.setScaleAndOpacityRandom();
-      // const setting = {
-      //   targets: '.' + this.tweetCardId,
-      //   translateX: [this.card.coordBefore.x, this.card.coord.x],
-      //   translateY: [this.card.coordBefore.y, this.card.coord.y],
-      //   scale: this.card.scale,
-      //   opacity: this.card.opacity,
-      //   easing: 'easeOutCubic'
-      // };
-
-      // 右から左へ流す
-      this.TIME = 10000;
-      this.card.setCoordLikeNico();
-      this.card.scale = 1;
-      this.card.opacity = 0.75;
-      const setting = {
-        targets: '.' + this.tweetCardId,
-        translateX: [this.card.coordBefore.x, this.card.coord.x],
-        translateY: [this.card.coordBefore.y, this.card.coord.y],
-        scale: this.card.scale,
-        opacity: this.card.opacity,
-        easing: 'linear',
-        duration: this.TIME,
-        delay: Math.random() * (this.TIME / 2)
-      };
-
+      const setting = this.animeSetting.set(AnimeMode.straight) as anime.AnimeAnimParams;
       anime(setting);
-
       this.display = 'block';
     }, this.TIME * 3 / 2);
   }
@@ -87,20 +49,9 @@ export class TweetCardComponent implements OnInit {
     this.card.setScale(0);
     this.card.setOpacity(0);
     clearInterval(this.interval);
-    this.zIndex = 10000;
   }
 
   onClick() {
-    const setting = {
-      targets: '.' + this.tweetCardCN,
-      translateX: [this.card.coordBefore.x, this.card.coord.x],
-      translateY: [this.card.coordBefore.y, this.card.coord.y],
-      scale: 0,
-      opacity: 0,
-      easing: 'easeOutCubic'
-    };
-
-    anime(setting);
     this.ds.Tweet = this.tweet;
     const modalRef = this.modal.open(
       DisplayCardComponent,
