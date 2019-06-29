a76ef39a8ea162bb60cb1d64711a2039a7691cfa
藤野孝政 <fujinotakamasa@fujinotakamasa-no-Mac-mini.local>
2019-06-29 15:07:54 +0900

    toolbar・sidebar変更

--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { DisplayTweetCardComponent } from './display-tweet-card/display-tweet-card.component';
 import { TweetCardComponent } from './tweet-card/tweet-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatToolbarModule, MatCardModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatSidenavModule} from '@angular/material';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayTweetCardComponent,
     TweetCardComponent,
+    SidebarComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,6 +26,7 @@ import { MatToolbarModule, MatCardModule } from '@angular/material';
     ReactiveFormsModule,
     FormsModule,
     MatToolbarModule,
+    MatSidenavModule,
   ],
   providers: [],
   entryComponents: [
