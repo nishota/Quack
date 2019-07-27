a472fb0c39684479b471c1d46a872ebdc51a2f30
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-07-28 02:38:00 +0900

    infomationConponent作成

--- front_end/src/app/infomation/infomation.component.ts
+++ front_end/src/app/infomation/infomation.component.ts
@@ -8,9 +8,29 @@ import { TweetGetterService } from '../tweet-getter.service';
 })
 export class InfomationComponent implements OnInit {
 
+  infos: Infomation[] = [];
+  infoNum = 3;
+
   constructor(private tg: TweetGetterService) { }
 
   ngOnInit() {
+    this.tg.getInfoFromAsset().subscribe(
+      (res: Infomation[]) => {
+        let count = 0;
+        res.forEach(content => {
+          if (count < this.infoNum) {
+            this.infos.push(content);
+          }
+          count++;
+        });
+      }
+    );
   }
+}
 
+export class Infomation {
+  date: string;
+  content: string;
+  url: string;
+  hasUrl: boolean;
 }
