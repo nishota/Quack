30e6369ca2ac4173eb5bc6b9bfaf6f7b340e311d
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-08-03 02:38:14 +0900

    infomationのURL修正、angular.json修正

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   devUrl: 'https://payload-01.herokuapp.com/json/twitredb/keyword',
   twitterUrl: 'https://twitter.com/',
-  infoUrl: ''
+  infoUrl: 'https://twitre.tomato-note.com/assets/infomation.json'
 };
