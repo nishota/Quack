bcbb40dce3283ffce13993bb65c0c781b1baf773
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-08-03 14:31:45 +0900

    スマホ版の表示調整

--- front_end/src/app/model/tweet.model.ts
+++ front_end/src/app/model/tweet.model.ts
@@ -1,4 +1,4 @@
-import { environment } from "../../environments/environment";
+import { environment } from '../../environments/environment';
 export class Tweet {
     private tweetId: string;
     private user: string;
