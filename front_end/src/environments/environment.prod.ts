17205781870f4d652eafb26bdbed475a74f7c3d1
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-12-07 13:42:44 +0900

    v1.0.0-next.1

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -9,6 +9,18 @@ export const environment = {
   socketUrl: 'ec2-18-182-7-70.ap-northeast-1.compute.amazonaws.com',
   pathString: '/backend/quack/socket.io',
 };
+// https用
+// export const environment = {
+//   production: true,
+//   twitterUrl: 'https://twitter.com/',
+//   twitterTrendUrl: 'http://twitter.com/search?q=%23',
+//   frontUrl: 'https://quack-teal.com/',
+//   infoUrl: 'https://quack-teal.com/assets/infomation.json',
+//   metadataUrl: 'https://quack-teal.com/assets/metadata.json',
+//   quackSystemUrl: 'https://quack-teal.com/assets/quack-system.json',
+//   socketUrl: 'ec2-18-182-7-70.ap-northeast-1.compute.amazonaws.com',
+//   pathString: '/backend/quack/socket.io',
+// };
 
 // electron用
 // export const environment = {
