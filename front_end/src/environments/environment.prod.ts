2e97380ac0202524551800ee299641d2305ebecd
Ninomiya Shota <a2038toh7s1124@gmail.com>
2019-12-07 13:51:56 +0900

    sp版トレンド表示修正、environment修正

--- front_end/src/environments/environment.prod.ts
+++ front_end/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   twitterUrl: 'https://twitter.com/',
-  twitterTrendUrl: 'http://twitter.com/search?q=%23',
-  frontUrl: 'https://quack-teal.com/',
-  infoUrl: 'https://quack-teal.com/assets/infomation.json',
-  metadataUrl: 'https://quack-teal.com/assets/metadata.json',
-  quackSystemUrl: 'https://quack-teal.com/assets/quack-system.json',
+  twitterTrendUrl: 'https://twitter.com/search?q=%23',
+  frontUrl: 'http://quack-teal.com/',
+  infoUrl: 'http://quack-teal.com/assets/infomation.json',
+  metadataUrl: 'http://quack-teal.com/assets/metadata.json',
+  quackSystemUrl: 'http://quack-teal.com/assets/quack-system.json',
   socketUrl: 'ec2-18-182-7-70.ap-northeast-1.compute.amazonaws.com',
   pathString: '/backend/quack/socket.io',
 };
